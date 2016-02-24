b2abc8ce31dacc2e0bde9e29b7ecc2973007369d
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2016-02-24 13:38:43 +0200

    Post modeli oluşturuldu

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,21 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+POST_STATUS = (
+    (0, u"Kapalı"),
+    (1, u"Yayında"),
+)
+
+class Post(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=POST_STATUS, default=1,)
+    published_date = models.DateTimeField()
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+
+    def __unicode__(self):
+        return u"%s" % self.title
