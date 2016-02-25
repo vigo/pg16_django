920c1c72b3306a2e42dae0f14799be7dc77a1b07
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2016-02-25 10:31:56 +0200

    crud için url yapısı yapıldı

--- pg16_django/urls.py
+++ pg16_django/urls.py
@@ -1,21 +1,15 @@
-"""pg16_django URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import url, static
 from django.contrib import admin
+
+from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.PostList.as_view(), name="post-list"),
+    url(r'^ekle/?$', views.PostCreate.as_view(), name="post-create"),
+    url(r'^detay/(?P<pk>\d+)/?$', views.PostDetail.as_view(), name="post-detail"),
+    url(r'^detay/(?P<pk>\d+)/duzenle/?$', views.PostUpdate.as_view(), name="post-update"),
+    url(r'^detay/(?P<pk>\d+)/sil/?$', views.PostDelete.as_view(), name="post-delete"),
 ]
+
